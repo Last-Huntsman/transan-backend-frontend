bc11ea3841d826b1577376bdcd2a43df00910be9
Egor <zuzik17@yandex.ru>
2026-03-10 10:15:00 +0300

    chore: init project with Spring Boot skeleton

---
+++ backend/src/main/java/com/transan/TransanApplication.java
@@ -0,0 +1,12 @@
+package com.transan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransanApplication.class, args);
+    }
+}
